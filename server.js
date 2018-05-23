ec2240b505b98309f7268c6c0271f397f4919375
Damian Marius <damian.mm@live.com>
2018-05-23 09:44:00 -0700

    added delete endpoint

--- server.js
+++ server.js
@@ -53,7 +53,9 @@ app.delete('/shopping-list/:id', (req, res) => {
   res.status(204).end();
 });
 
-
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
 // if okay, add new item, and return it with a status 201.
@@ -72,10 +74,12 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe \`${req.params.id}\``);
+  res.status(204).end();
 
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-})
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
